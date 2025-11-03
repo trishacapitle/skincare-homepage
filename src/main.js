696d9af72ecd34e5543e9475b6b19fb0c062cf35
ashdev <trishacapitle01@gmail.com>
2025-11-03 17:53:08 +0800

    feat: js function and additional samples for carousel

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./scss/main.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+	const carousel = document.querySelector(".fan-reviews__carousel");
+	new Flickity(carousel, {
+		wrapAround: true,
+		autoPlay: 3000,
+		pageDots: false,
+		prevNextButtons: false,
+	});
+});
